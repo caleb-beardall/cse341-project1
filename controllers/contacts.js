1dec98c9425663af21ddf83a140c2a0a29e442ae
caleb-beardall <calebbeardall@byui.edu>
2025-11-03 22:13:46 -0700

    Completed Steps 1 - 5 of W01 Project: Contacts Part 1

---
+++ controllers/contacts.js
@@ -0,0 +1,29 @@
+const mongodb = require('../db/connect');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = async (req, res, next) => {
+    const result = await mongodb
+        .getDb()
+        .db('project1')
+        .collection('contacts')
+        .find();
+    result.toArray().then((lists) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists);
+    });
+};
+
+const getSingle = async (req, res, next) => {
+    const contactId = new ObjectId(req.params.id);
+    const result = await mongodb
+        .getDb()
+        .db('project1')
+        .collection('contacts')
+        .find({ _id: contactId });
+    result.toArray().then((lists) => {
+        res.setHeader('Content-Type', 'application/json');
+        res.status(200).json(lists[0]);
+    });
+};
+
+module.exports = { getAll, getSingle };
